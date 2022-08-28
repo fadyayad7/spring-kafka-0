6621de3b48bfa54a9b60574e996ef3e6c10c9fe6
fadyayad7 <fady.ayad7@gmail.com>
2022-08-28 16:26:43 +0200

    initial commit

---
+++ src/main/java/dev/fadyayad/springkafka0/SpringKafka0Application.java
@@ -0,0 +1,13 @@
+package dev.fadyayad.springkafka0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafka0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafka0Application.class, args);
+    }
+
+}
---
+++ src/test/java/dev/fadyayad/springkafka0/SpringKafka0ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fadyayad.springkafka0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafka0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
